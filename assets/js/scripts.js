bb4f49964ba9fbe6e506d92568eb59745e89e5bc
Ravindra barthwal <rtechshow@gmail.com>
2016-07-18 16:40:42 +0530

    Fix: Change Menu
    Menu changed from content sliding to menu overlay.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,14 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
 	$('#mySidenav').css("right", "0px");
-	$('#container-main').css("margin-left", "-300px");
+	$('.overlay-menu').show();
 	$(".icon-menu").hide();
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
     $('#mySidenav').css("right", "-300px");
-	$('#container-main').css("margin-left", "0");
+	$('.overlay-menu').hide();
 	$(".icon-menu").show();
 }
 
